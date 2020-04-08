d9dce34b7f55676a1335a6ce6b82a6e4d6a8c2f0
IZABELA MATOS ALFENAS <izabela.m.alfenas@gmail.com>
2020-04-08 15:58:14 -0300

    Fixing some bugs

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const localUrl = 'http://localhost:3333'
 
 const api = axios.create({
-    baseURL: process.env.REACT_APP_API_URL || localUrl
+    baseURL: localUrl
 })
 
 export default api
